3b1e03222b2f7d4afd0b312681154923cd622b2c
pkonwledge <xyz@gmail.com>
2024-11-07 01:25:44 +0200

    first commit

--- src/components/3-main/myProjects.js
+++ src/components/3-main/myProjects.js
@@ -4,7 +4,7 @@ export const myProjects = [
     projectDesc:
       "This is a web-based system that deals with the issue of managing and booking appointments according to user’s choice/necessity. Patients can select doctors based on their medical needs and they can look at their details and reviews to learn more. Hence this project offers an effective solution where users can view various booking slots available and select the preferred date and time. This reduces physical waiting time and not only saves time for the users but also makes the appointment process more efficient",
     category: ["react"],
-    imgPath: "public/images/doctor-appointment.png",
+    imgPath: "./assets/doctor-appointment.png",
     demoLink: "https://shifaa.vercel.app/",
     repoLink: "https://github.com/Mmostafa1999/Doctors-Appointments",
   },
@@ -13,7 +13,7 @@ export const myProjects = [
     projectDesc:
       "The Todo-list Application is a robust task management tool built with React.js and integrated with Firebase for authentication and database services. This application ensures a secure and personalized experience, requiring users to sign in or sign up to access their tasks. It's designed to help users efficiently manage their daily responsibilities with a range of interactive features",
     category: ["react"],
-    imgPath: "../../../public/images/todo-list.png",
+    imgPath: "./assets/todo-list.png",
     demoLink: "https://todo-list-jet-five-39.vercel.app/",
     repoLink: "https://github.com/Mmostafa1999/Todo-list",
   },
@@ -22,7 +22,7 @@ export const myProjects = [
     projectDesc:
       "Admin Dashboard website is a frontend application built using React with Vite. It incorporates Material UI for components. This dashboard simplifies data handling while offering flexibility and scalability, streamline user management and data administration tasks. In addition to that dynamic table and charts views, and filtering options, this dashboard is an essential tool for efficient resource management.",
     category: ["react"],
-    imgPath: "../../../public/images/dashboard.png",
+    imgPath: "./assets/dashboard.png",
     demoLink: "https://admin-dashboard-52b2d.firebaseapp.com/",
     repoLink: "https://github.com/Mmostafa1999/Admin-Dashboard",
   },
@@ -32,7 +32,7 @@ export const myProjects = [
     projectDesc:
       "The Hotel Template Website provides a seamless experience for hotel owners to showcase their property and services online. With its clean and intuitive design, visitors can easily navigate through various sections of the website to explore rooms, make reservations, and get in touch with the hotel.",
     category: ["react"],
-    imgPath: "../../../public/images/hotel.png",
+    imgPath: "./assets/hotel.png",
     demoLink: "https://hotel-templete-webiste.netlify.app/",
     repoLink: "https://github.com/Mmostafa1999/STETHO-Hotel-",
   },
@@ -42,7 +42,7 @@ export const myProjects = [
     projectDesc:
       "This is a fully responsive social media web application built using React.js and material UI. The app is look a lot like user interface of facebook app. the greatest focus in designing this app was training on using material Ui.",
     category: ["react"],
-    imgPath: "../../../public/images/mini-facebook.png",
+    imgPath: "./assets/mini-facebook.png",
     demoLink: "https://social-mediaa-react-app.netlify.app/",
     repoLink: "https://github.com/Mmostafa1999/Mini-Facebook",
   },
@@ -52,7 +52,7 @@ export const myProjects = [
     projectDesc:
       "This project is a static web application built with React, showcasing the design and layout for an interactive GPT-3 AI interface. This project demonstrates my skills in building static front-end applications with a focus on responsive design and modern web deployment by using react components",
     category: ["react"],
-    imgPath: "../../../public/images/gpt3.png",
+    imgPath: "./assets/gpt3.png",
     demoLink: "https://gpt-3react-app.netlify.app/",
     repoLink: "https://github.com/Mmostafa1999/GPT-3-React-App",
   },
@@ -62,7 +62,7 @@ export const myProjects = [
     projectDesc:
       "This Website showcases template design for Software company, all designed to be responsive for mobile users. colors change is covered to get users with a variety of colors, can enjoy enhanced readability and a visually pleasing experience.",
     category: ["js"],
-    imgPath: "../../../public/images/software-company.jpg",
+    imgPath: "./assets/software-company.jpg",
     demoLink: "https://mmostafa1999.github.io/website-company/",
     repoLink: "https://github.com/Mmostafa1999/website-company",
   },
@@ -72,7 +72,7 @@ export const myProjects = [
     projectDesc:
       "This project provides a fun and interactive way for users to play drum sounds using their mouse or keyboard. It demonstrates basic DOM manipulation, event handling, and audio playback in a web environment.",
     category: ["js"],
-    imgPath: "../../../public/images/drum-kit.png",
+    imgPath: "./assets/drum-kit.png",
     demoLink: "https://mmostafa1999.github.io/Drum-Kit/",
     repoLink: "https://github.com/Mmostafa1999/Drum-Kit",
   },
@@ -82,7 +82,7 @@ export const myProjects = [
     projectDesc:
       "This project is a comprehensive CRUD (Create, Read, Update, Delete) application designed to streamline product management. Built with a focus on user experience, the system allows users to efficiently manage their product inventory through a clean and intuitive interface.",
     category: ["js"],
-    imgPath: "../../../public/images/cruds-system.png",
+    imgPath: "./assets/cruds-system.png",
     demoLink: "https://mmostafa1999.github.io/Product-management-system/",
     repoLink: "https://github.com/Mmostafa1999/Product-management-system",
   },
@@ -92,7 +92,7 @@ export const myProjects = [
     projectDesc:
       "The TinDog project is a responsive website crafted specifically for dog lovers looking to connect with others in their community. This application showcases the powerful capabilities of Bootstrap, allowing for a clean, modern design that adapts seamlessly to various screen sizes.",
     category: ["bootstrap"],
-    imgPath: "../../../public/images/tindog.png",
+    imgPath: "./assets/tindog.png",
     demoLink: "https://mmostafa1999.github.io/TinDog/",
     repoLink: "https://github.com/Mmostafa1999/TinDog",
   },
@@ -102,7 +102,7 @@ export const myProjects = [
     projectDesc:
       "This project, built using HTML and CSS, showcases my skills in layout design and responsiveness. I utilized CSS Flexbox, Grid, and Positioning techniques to create a structured, visually appealing, and user-friendly interface. Mealify served as a platform to test and refine my proficiency in CSS, focusing on both modern layout methods and fundamental styling principles.",
     category: ["css"],
-    imgPath: "../../../public/images/mealify.png",
+    imgPath: "./assets/mealify.png",
     demoLink: "https://mmostafa1999.github.io/Mealify/",
     repoLink: "https://github.com/Mmostafa1999/Mealify",
   },
